7debc92b34eda095059fe884222c3a4d1c5c688c
Bernard Delhez <bernarddelhez@gmail.com>
2016-08-19 14:29:20 +0200

    update version to 9

--- __openerp__.py
+++ __openerp__.py
@@ -1,6 +1,6 @@
 {
     'name': "Worklog management improvements",
-    'version': '1.0',
+    'version': '9.0.1.0',
     'depends': ['hr_timesheet','project_timesheet', 'project_issue_sheet'],
     'author': "Bernard DELHEZ, AbAKUS it-solutions SARL",
     'website': "http://www.abakusitsolutions.eu",
